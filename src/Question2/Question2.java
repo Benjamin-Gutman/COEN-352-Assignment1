f2534f94c002377556da66a3e874bc4172a173a2
Benjamin Gutman <benjamingutman@icloud.com>
2025-05-24 08:47:29 -0400

    Started Question 2

---
+++ src/Question2/Question2.java
@@ -0,0 +1,100 @@
+package Question2;
+
+public class Question2{
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
+
+
+class LinkedList{
+	Node First;
+	Node Rear;
+	int Size = 0;
+	
+	private class Node{
+		int Data;
+		Node Next;
+	}
+	
+	void addToFront(int newFront) {
+		Node temp = new Node();
+		temp.Data = newFront;
+		temp.Next = First;
+		First = temp;
+		Size++;
+		if (Size == 1) {
+			Rear = First;
+		}
+		
+	}
+	void addToRear(int newRear) {
+		if (Size == 0) {
+			Node temp = new Node();
+			temp.Data = newRear;
+			temp.Next = First;
+			First = temp;
+			Size++;
+			Rear = First;
+		}
+		else{
+			Node temp = new Node();
+			temp.Next = null;
+			temp.Data = newRear;
+			Rear.Next = temp;
+			Rear = temp;
+			Size++;
+		}
+	}
+	void printList() {
+		Node Current = new Node();
+		Current = First;
+		while (Current != null){
+		Node temp = new Node();
+		temp = Current.Next;
+		System.out.println(Current.Data);
+		Current = temp;
+		}
+	}
+	
+	void DeleteRange(int start, int end) {
+		if (start > end) {
+			System.out.println("Boundary Error");
+			return;
+		}
+		if (end > Size) {
+			System.out.println("Out of Range Error");
+			return;
+		}
+		
+		Node temp = First;
+		for (int i = 1; i <start-1; i++) {
+			temp = temp.Next;
+		}
+		Node temp2 = First;
+		for (int i = 1; i <=end; i++) {
+			temp2 = temp2.Next;
+		}
+		if(start == 1) {
+			First = temp2;
+		}
+		else {
+			temp.Next = temp2;
+		}
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
