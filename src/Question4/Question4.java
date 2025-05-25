a45cd0f28021320481efa3dc587921592822cd86
Benjamin Gutman <benjamingutman@icloud.com>
2025-05-25 18:33:10 -0400

    Finished Question 4

--- src/Question4/Question4.java
+++ src/Question4/Question4.java
@@ -2,7 +2,9 @@ package Question4;
 
 public class Question4{
 	public static void main(String[] args) {
-		
+		josephusQueue Test = new josephusQueue();
+		Test.createList(7);
+		Test.printOrderOfElimination(2);
 	}
 }
 
@@ -22,12 +24,28 @@ class josephusQueue{
 			temp.Next = null;
 			if (i == 0) {
 				First = temp;
+				Last = temp;
 			}
+			Last.Next = temp;
 			Last = temp;
 		}
 	}
 	
 	void printOrderOfElimination(int m) {
+		while (First != Last) {
+			for (int i = 0; i<m-1;i++) {
+				Last.Next = First;
+				First = First.Next;
+				Last = Last.Next;
+				Last.Next = null;
+			}
+			System.out.println(First.Data);
+			if (First.Next != null) {
+				First = First.Next;
+			}
+		}
+		System.out.println(First.Data);
+		
 		
 	}
 }
